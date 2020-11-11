80fc5b62f2ae68c268682f211822c48cf551bb44
foamyguy <foamyguy@gmail.com>
2020-11-10 20:02:54 -0600

    retry initial request if it fails. switching to displayio driver

--- code.py
+++ code.py
@@ -1,14 +1,16 @@
 import gc
 import ipaddress
 import wifi
-import adafruit_requests
 import time
 import ssl
 import socketpool
 import board
-import busio as io
+import terminalio
+import displayio
+import adafruit_requests
+from adafruit_display_text import label
 from wrap_nicely import wrap_nicely
-import adafruit_ssd1306
+import adafruit_displayio_ssd1306
 
 from secrets import secrets
 
@@ -26,43 +28,61 @@ def display_text(text, line):
     -------
     None
     """
-    oled.fill(0)
+
     text = str(text)
-    oled.text(text, 0, line, 1)
-    #oled.text('World', 0, 10)
-    oled.show()
+    output_label.text = " "
+    output_label.text = text
 
 
 # Instantiate i2c object
-i2c = io.I2C(board.SCL, board.SDA)
+i2c = board.I2C()
 
+displayio.release_displays()
 # Instantiate OLED object
-oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c, addr=0x3c)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3C)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=32, rotation=180)
+
+output_label = label.Label(terminalio.FONT, color=0xFFFFFF, max_glyphs=30 * 4)
+output_label.anchor_point = (0, 0)
+output_label.anchored_position = (0, 0)
+
+display.show(output_label)
 
 # Setup wifi and connection
 print(wifi.radio.connect(secrets['ssid'], secrets['password']))
 print('ip', wifi.radio.ipv4_address)
+display_text("ip: {}".format(wifi.radio.ipv4_address), 0)
 ipv4 = ipaddress.ip_address('8.8.8.8')
-print('ping', wifi.radio.ping(ipv4))
+ping_result = wifi.radio.ping(ipv4)
+print('ping', ping_result)
+display_text("ping: {}".format(ping_result), 0)
 pool = socketpool.SocketPool(wifi.radio)
 requests = adafruit_requests.Session(pool, ssl.create_default_context())
 
 # Set up where we'll be fetching data from
 DATA_SOURCE = 'https://hacker-news.firebaseio.com/v0/topstories.json'
 
-# Perform a GET on the DATA_SOURCE and instantiate into a response object
-print('Fetching DATA_SOURCE')
-
 # Garbage collect before our GET request
 gc.collect()
+suceed = False
 
-# Create our response and DATA objects
-response = requests.get(DATA_SOURCE)
-DATA = response.json()
-response.close()
+while not suceed:
+    # Perform a GET on the DATA_SOURCE and instantiate into a response object
+    display_text('Fetching DATA_SOURCE', 0)
+    print('Fetching DATA_SOURCE')
+    try:
+        # Create our response and DATA objects
+        response = requests.get(DATA_SOURCE)
+        DATA = response.json()
+        response.close()
+        suceed = True
+    except OSError as e:
+        print(e)
+        display_text("OS Error. Retrying", 0)
+        time.sleep(5)
 
-# Garbage collect after our GET request
-gc.collect()
+    # Garbage collect after our GET request
+    gc.collect()
 
 while True:
     try:
